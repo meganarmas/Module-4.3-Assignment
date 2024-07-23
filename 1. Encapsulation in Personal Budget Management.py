f0e2f5ee163bb5ed120c178a9f66393df492c129
meganarmas <meganarmas@gmail.com>
2024-07-23 15:44:42 -0500

    fixing errors

--- 1. Encapsulation in Personal Budget Management.py
+++ 1. Encapsulation in Personal Budget Management.py
@@ -52,28 +52,26 @@ class BudgetCategory:
 # expenses to a category and adjust the budget accordingly. 
 # - Validate the expense amount before making deductions from the budget.
 
-    def add_expenses(self, amount):
+    def add_expense(self, amount):
         if amount <= self.__remaining_budget:
             self.__remaining_budget -= amount
             self.__expenses.append(amount)
         else:
             raise ValueError("Expenses should be positive number.")
     
-
-
 # Expected Outcome: Ability to track expenses per category and update the remaining budget safely.
 
 # Task 4: Display Budget Details Create a method to display the details of a 
 # budget category, including the name, allocated budget, and remaining budget after expenses.
   
-    def display_cat(self):
+    def display_category_summary(self):
         print(f"Category: {self.get_category()}")
         print(f"Allocated Budget {self.get_allocated_budget()}")
-        expenses = 
-
-    def budget_details(self):
-        print(f"Category: {self.__category_name}, Allocated Budget: {self.__allocated_budget}, Remaining Budget: {self.__remaining_budget}")
+        print(f"Remaining Budget: {self.__expenses}")
 
 # Expected Outcome: Users can view a summary of each budget category, showcasing encapsulation in action.
 
+food_category = BudgetCategory("Food", 500)
+food_category.add_expense(100)
+food_category.display_category_summary()
 
--- budget_main.py
+++
@@ -1,3 +0,0 @@
-import Enc
-
-def main():
